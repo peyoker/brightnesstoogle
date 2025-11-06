0c0f98a490c2d3210d88d9a80f83025be78249f8
peyoker <peyoker@users.noreply.github.com>
2025-11-07 00:10:30 +0100

    Refactor and reorganize Program.cs structure

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using System.Windows.Forms;
 using System.Drawing;
 using Microsoft.Win32;
 using System.Management;
-using NoBright.Properties;
 
 namespace NoBright
 {
@@ -486,6 +485,7 @@ namespace BrightnessToggle.Properties
     }
 
 }
+
 
 
 
